a31ba76e5b48575e4459bfb9c1bcd297a3b1acfd
Joentze <Joentze01@gmail.com>
2021-07-13 05:45:29 -0700

    minor

--- quark-api.js
+++ quark-api.js
@@ -3,7 +3,7 @@
 //sends uri for png
 window.addEventListener('message',event =>{
     console.log('receiving a message')
-
+    console.log(event.data)
     if(event.data[fn] == "downloadPNG"){
         
         let uri = calculator.screenshot()
